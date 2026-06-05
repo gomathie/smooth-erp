198c5ad737509b5e516c58d0c4ee6dd4d0423c6e
gomathie <gomathie@byui.edu>
2026-06-06 00:02:56 +0300

    Fix DataTables invalid JSON response in datatable-products.ajax.php and ajax/datatable-sales.ajax.php

--- ajax/datatable-sales.ajax.php
+++ ajax/datatable-sales.ajax.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 
 require_once "../controllers/products.controller.php";
 require_once "../models/products.model.php";
@@ -9,83 +9,57 @@ require_once "../models/customers.model.php";
 
 class productsTableSales{
 
-	/*=============================================
- 	 SHOW PRODUCTS TABLE
-  	=============================================*/ 
-	public function showProductsTableSales(){
+/*=============================================
+  SHOW PRODUCTS TABLE
+ =============================================*/ 
+public function showProductsTableSales(){
 
-		$item = null;
-		$value = null;
-		$order = "id";
+header('Content-Type: application/json; charset=utf-8');
 
-		$products = ControllerProducts::ctrShowProducts($item, $value, $order);
+$item = null;
+$value = null;
+$order = "id";
 
-		if(count($products) == 0){
+$products = ControllerProducts::ctrShowProducts($item, $value, $order);
 
-			$jsonData = '{"data":[]}';
+$data = [];
 
-			echo $jsonData;
+for($i = 0; $i < count($products); $i++){
 
-			return;
-		}
+/*=============================================
+We bring the image
+=============================================*/
+$image = "<img src='" . $products[$i]["image"] . "' width='40px'>";
 
-		$jsonData = '{
-			"data":[';
-
-				for($i=0; $i < count($products); $i++){
-
-
-					/*=============================================
-					We bring the image
-					=============================================*/
-					
-					$image = "<img src='".$products[$i]["image"]."' width='40px'>";
-
-					/*=============================================
-					Stock
-					=============================================*/
-				  	
-				  	if($products[$i]["stock"] <= 10){
-
-		  				$stock = "<button class='btn btn-danger'>".$products[$i]["stock"]."</button>";
-
-		  			}else if($products[$i]["stock"] > 11 && $products[$i]["stock"] <= 15){
-
-		  				$stock = "<button class='btn btn-warning'>".$products[$i]["stock"]."</button>";
-
-		  			}else{
-
-		  				$stock = "<button class='btn btn-success'>".$products[$i]["stock"]."</button>";
-
-		  			}
-
-		  			/*=============================================
-		 	 		ACTION BUTTONS
-		  			=============================================*/ 
-
-		  			$buttons =  "<div class='btn-group'><button class='btn btn-primary addProductSale recoverButton' idProduct='".$products[$i]["id"]."'><i class='fa fa-plus'></i></button></div>";
-
-
-
-					$jsonData .='[
-						"'.($i+1).'",
-						"'.$image.'",
-						"'.$products[$i]["code"].'",
-						"'.$products[$i]["description"].'",
-						"'.$stock.'",
-						"'.$buttons.'"
-					],';
-				}
-
-				$jsonData = substr($jsonData, 0, -1);
-				$jsonData .= '] 
-
-			}';
-
-		echo $jsonData;
-	}
+/*=============================================
+Stock
+=============================================*/
+if($products[$i]["stock"] <= 10){
+$stock = "<button class='btn btn-danger'>" . $products[$i]["stock"] . "</button>";
+}else if($products[$i]["stock"] > 11 && $products[$i]["stock"] <= 15){
+$stock = "<button class='btn btn-warning'>" . $products[$i]["stock"] . "</button>";
+}else{
+$stock = "<button class='btn btn-success'>" . $products[$i]["stock"] . "</button>";
 }
 
+/*=============================================
+ ACTION BUTTONS
+ =============================================*/ 
+$buttons = "<div class='btn-group'><button class='btn btn-primary addProductSale recoverButton' idProduct='" . $products[$i]["id"] . "'><i class='fa fa-plus'></i></button></div>";
+
+$data[] = [
+($i + 1),
+$image,
+$products[$i]["code"],
+$products[$i]["description"],
+$stock,
+$buttons
+];
+}
+
+echo json_encode(["data" => $data]);
+}
+}
 
 /*=============================================
 ACTIVATE PRODUCTS TABLE
